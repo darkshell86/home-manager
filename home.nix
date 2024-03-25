25b78790d8b6b9382b2e168bfa124523a3813de6
Dark Shell <134949053+darkshell86@users.noreply.github.com>
2024-03-25 16:50:43 +0000

    disable wezterm transparency because of stream

--- home.nix
+++ home.nix
@@ -179,8 +179,8 @@
       -- This is where you actually apply your config choices
       config.color_scheme = 'Catppuccin Macchiato'
       config.hide_tab_bar_if_only_one_tab = true
-      config.window_background_opacity = 0.95
-      config.macos_window_background_blur = 20
+      --config.window_background_opacity = 0.95
+      --config.macos_window_background_blur = 20
       config.window_decorations = "RESIZE"
       config.default_prog = { 'zsh', '-i', '-c', 'nu' }
 
